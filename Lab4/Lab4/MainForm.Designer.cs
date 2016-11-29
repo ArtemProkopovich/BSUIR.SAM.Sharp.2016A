fdd215344e70e492d800b8cad13f84b517854965
Artsiom_Prakapovich <Artsiom_Prakapovich@EPBYMINW6631>
2016-11-29 12:39:49 +0300

    l4

--- Lab4/Lab4/MainForm.Designer.cs
+++ Lab4/Lab4/MainForm.Designer.cs
@@ -28,20 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.inputNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.okButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.inputNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputNumericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(190, 12);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(387, 340);
+            this.resultTextBox.TabIndex = 0;
+            // 
+            // inputNumericUpDown
+            // 
+            this.inputNumericUpDown.DecimalPlaces = 1;
+            this.inputNumericUpDown.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.inputNumericUpDown.Location = new System.Drawing.Point(12, 28);
+            this.inputNumericUpDown.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.inputNumericUpDown.Name = "inputNumericUpDown";
+            this.inputNumericUpDown.Size = new System.Drawing.Size(169, 20);
+            this.inputNumericUpDown.TabIndex = 1;
+            this.inputNumericUpDown.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Интенсивность входного потока";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Интенсивность выходного потока";
+            // 
+            // outputNumericUpDown
+            // 
+            this.outputNumericUpDown.DecimalPlaces = 1;
+            this.outputNumericUpDown.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.outputNumericUpDown.Location = new System.Drawing.Point(12, 79);
+            this.outputNumericUpDown.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.outputNumericUpDown.Name = "outputNumericUpDown";
+            this.outputNumericUpDown.Size = new System.Drawing.Size(169, 20);
+            this.outputNumericUpDown.TabIndex = 3;
+            this.outputNumericUpDown.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            65536});
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(55, 124);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 5;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(414, 364);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(589, 364);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.outputNumericUpDown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputNumericUpDown);
+            this.Controls.Add(this.resultTextBox);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.inputNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputNumericUpDown)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.NumericUpDown inputNumericUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown outputNumericUpDown;
+        private System.Windows.Forms.Button okButton;
     }
 }
 
--- Lab4/Lab4/MainForm.cs
+++ Lab4/Lab4/MainForm.cs
@@ -16,5 +16,111 @@ namespace Lab4
         {
             InitializeComponent();
         }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            Simulator simulator = new Simulator((double)inputNumericUpDown.Value, (double)outputNumericUpDown.Value);
+            var result = simulator.Simulate();
+
+            
+            var maxState = result.Max(t => t.Buffer + (t.IsBusy ? 1 : 0));
+            List<long> propCount = new List<long>();           
+            resultTextBox.Text = "";
+            long allTime = result.Sum(t => t.Period);
+            for (int i = 0; i <= maxState; i++)
+            {
+                propCount.Add(result.Where(t => t.Buffer + (t.IsBusy ? 1 : 0) == i).Sum(t => t.Period));
+            }
+
+            double averageBufferLength = 0.0;
+            var maxBuffer = result.Max(t => t.Buffer);
+            for (int i = 0; i <= maxBuffer; i++)
+            {
+                averageBufferLength += 1.0*result.Where(t => t.Buffer == i).Sum(t => t.Period)*i/allTime;
+            }
+
+            resultTextBox.Text += $"Средняя длина очереди: {averageBufferLength:0.00000}" + Environment.NewLine;
+
+            resultTextBox.Text += $"Все время обработки: {allTime}" + Environment.NewLine;
+            for (int i = 0; i <= maxState; i++)
+            {
+                resultTextBox.Text += $"Вероятность состояния {i:00}: {1.0*propCount[i]/allTime:0.00000}" + Environment.NewLine;
+            }
+        }
+
+
+        public class Simulator
+        {
+            private const int Accuracy = 100;
+            public const int TaskCount = 1000000;
+
+
+            private static Random random = new Random();
+            private double inputIntensity;
+            private double outputIntensity;
+
+            public Simulator(double inputIntensity, double outputIntensity)
+            {
+                this.inputIntensity = inputIntensity;
+                this.outputIntensity = outputIntensity;
+            }
+
+            public List<State> Simulate()
+            {
+                List<State> result = new List<State>();
+                long finishProcessingTime = 0;
+                long nextTaskTime = 0;
+                long currentTime = 0;
+                long prevTime = 0;
+                State systemState = new State();
+                int taskCount = TaskCount;
+                int completedTasks = 0;
+
+                while (completedTasks != TaskCount)
+                {
+                    if (nextTaskTime == currentTime)
+                    {                       
+                        if (taskCount != 0)
+                        {
+                            systemState.Buffer++;
+                            nextTaskTime += random.Next(0, 2*Convert.ToInt32(Accuracy/inputIntensity));
+                        }
+                        taskCount--;
+                    }
+                    if (finishProcessingTime == currentTime || !systemState.IsBusy)
+                    {
+                        if (systemState.IsBusy)
+                            completedTasks++;                       
+                        if (systemState.Buffer > 0)
+                        {                           
+                            systemState.Buffer--;
+                            systemState.IsBusy = true;
+                            finishProcessingTime += Convert.ToInt32(Accuracy/outputIntensity);
+                        }
+                        else
+                        {
+                            finishProcessingTime = nextTaskTime;
+                            systemState.IsBusy = false;
+                        }                       
+                    }
+                    result.Add(new State()
+                    {
+                        Buffer = systemState.Buffer,
+                        IsBusy = systemState.IsBusy,
+                        Period = currentTime - prevTime
+                    });
+                    prevTime = currentTime;
+                    currentTime = Math.Min(nextTaskTime, finishProcessingTime);
+                }
+                return result;
+            }
+        }
+
+        public class State
+        {
+            public int Buffer { get; set; }
+            public bool IsBusy { get; set; }
+            public long Period { get; set; }
+        }
     }
 }
